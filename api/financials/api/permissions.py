bf27e7a5056bdcc3750537f8a9235ebb2c8d6689
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-02 07:52:12 +0100

    fix: ensure permission allows only authenticated users

--- api/financials/api/permissions.py
+++ api/financials/api/permissions.py
@@ -2,5 +2,10 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
     def has_object_permission(self, request, view, obj) -> bool:
         return obj.user == request.user
