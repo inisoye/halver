7c1e844d5a398eaf40b3d90e4fea3b3e21d7f18a
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-06 09:26:57 +0100

    feat: configure celery with redis as broker and result backend as well

--- api/core/settings.py
+++ api/core/settings.py
@@ -258,3 +258,14 @@ REST_AUTH_SERIALIZERS = {
 REST_AUTH_REGISTER_SERIALIZERS = {
     "REGISTER_SERIALIZER": "accounts.serializers.CustomRegisterSerializer",
 }
+
+
+# Celery configuration
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_LOADER = 'core.celery.app'
