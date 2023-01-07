55d8d0bf6f8388e88809b91358be734810a277a0
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-07 10:24:06 +0100

    feat: change filter method for overdue actions

--- api/bills/tasks.py
+++ api/bills/tasks.py
@@ -14,13 +14,14 @@ from .models import Action
 @transaction.atomic
 def check_and_update_actions_and_bill_participants():
     try:
-        # Get all actions with a bill due date that is past today
-        overdue_actions = Action.objects.filter(
-            bill__due_date__lt=datetime.date.today()
+        # Get all actions with a bill due date that is past today and with statuses
+        # that are not already marked as completed
+        overdue_and_incomplete_actions = Action.objects.filter(
+            bill__due_date__lt=datetime.date.today(), status__ne="completed"
         )
 
         # Update the status of each overdue action to "overdue"
-        for action in overdue_actions:
+        for action in overdue_and_incomplete_actions:
             # Retrieve a fresh copy of the action and bill participant objects
             action = Action.objects.get(pk=action.pk)
             bill_participant = action.bill_participant
