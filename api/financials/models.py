d317559b827ef43749753b0e12057ddf5391f6a6
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-09 06:06:18 +0100

    refactor: move create file method to util

--- api/financials/models.py
+++ api/financials/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 from core.models import AbstractTimeStampedUUIDModel
-from core.utils import get_user_by_id
+from financials.utils.cards import create_card
 from financials.utils.common import set_as_default
 
 User = settings.AUTH_USER_MODEL
@@ -95,33 +95,7 @@ class UserCard(AbstractTimeStampedUUIDModel, models.Model):
             webhook_data: Data returned through webhook by Paystack
         """
 
-        # TODO Add a method for adding new cards on paystack.
-        # This should initialize transactions, should be refundable and should be
-        # the only non-recurring paystack transaction for every user.
-
-        # The user id added in the metadata of the initialized card transaction
-        user_id = webhook_data["metadata"]["user_id"]
-        # The user for whom the card is created
-        user = get_user_by_id(user_id)
-        customer_data = webhook_data["customer"]
-        authorization_data = webhook_data["authorization"]
-
-        defaults = {
-            "account_name": authorization_data["account_name"],
-            "authorization_code": authorization_data["authorization_code"],
-            "bank": authorization_data["bank"],
-            "first6": authorization_data["bin"],
-            "card_type": authorization_data["card_type"],
-            "country_code": authorization_data["country_code"],
-            "email": customer_data["email"],
-            "exp_month": authorization_data["exp_month"],
-            "exp_year": authorization_data["exp_year"],
-            "last4": authorization_data["last4"],
-            "user": user,
-            "signature": authorization_data["signature"],
-        }
-
-        cls.objects.update_or_create(**defaults, defaults=defaults)
+        create_card(cls, webhook_data)
 
 
 class TransferRecipient(AbstractTimeStampedUUIDModel, models.Model):
