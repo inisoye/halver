5e8aeb84ff3d4676f80680d81f632d49df89e089
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-22 07:22:24 +0100

    refactor: make celery setup match docs explanation

--- api/core/celery.py
+++ api/core/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 
@@ -16,3 +16,8 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Discover and load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self) -> None:
+    print(f"Request: {self.request!r}")
