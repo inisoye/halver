a0d4f49fa32ec81db739ef34eb0f087b6baa519d
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-26 12:06:24 +0100

    feat: update core utils

--- api/core/utils.py
+++ api/core/utils.py
@@ -2,15 +2,25 @@ import datetime
 
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.utils import timezone
 
 from accounts.models import CustomUser
 
 
-def remove_underscores(string: str) -> str:
-    return string.replace("_", " ")
-
-
 def get_user_by_id(user_id) -> CustomUser:
+    """
+    Get user by ID.
+
+    Args:
+        user_id (str): The ID of the user to retrieve.
+
+    Returns:
+        CustomUser: The user with the specified ID.
+
+    Raises:
+        ObjectDoesNotExist: If the user with the specified ID does not exist.
+    """
+
     User = get_user_model()
 
     try:
@@ -33,3 +43,28 @@ def validate_date_not_in_past(date: datetime.datetime | None, date_name: str) ->
 
     if date < datetime.date.today():
         raise ValidationError(f"{date_name} cannot be in the past.")
+
+
+def remove_underscores(string: str) -> str:
+    """
+    Removes underscores from a string.
+
+    Args:
+        string (str): The string to remove underscores from.
+
+    Returns:
+        str: The string with underscores removed.
+    """
+
+    return string.replace("_", " ")
+
+
+def get_one_week_from_now():
+    """
+    Get the date and time one week from now
+
+    Returns:
+        datetime: the date and time one week from now
+    """
+
+    return timezone.now() + datetime.timedelta(weeks=1)
