301b9a58a4b3d7b59dc6267fca6f79c49c38c6b6
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-01-31 01:37:15 +0100

    refactor: hide recurring action creator task temporarily

--- api/bills/tasks.py
+++ api/bills/tasks.py
@@ -1,30 +1,30 @@
-import datetime
+# import datetime
 
-from celery import periodic_task
-from django.db import transaction
+# from celery.decorators import periodic_task
+# from django.db import transaction
 
-from bills.models import Action
+# from bills.models import Action
 
 # TODO Add flower to track all tasks across application
 
 
-@periodic_task(
-    run_every=datetime.timedelta(hours=12),
-    default_retry_delay=3600,
-    max_retries=2,
-)
-@transaction.atomic
-def update_overdue_statuses():
-    try:
-        # Get all actions with pending statuses and deadlines in the past
-        overdue_and_incomplete_actions = Action.objects.filter(
-            bill__deadline__lt=datetime.date.today(), status="pending"
-        )
+# @transaction.atomic
+# @periodic_task(
+#     run_every=datetime.timedelta(hours=12),
+#     default_retry_delay=3600,
+#     max_retries=2,
+# )
+# def update_overdue_statuses():
+#     try:
+#         # Get all actions with pending statuses and deadlines in the past
+#         overdue_and_incomplete_actions = Action.objects.filter(
+#             bill__deadline__lt=datetime.date.today(), status="pending"
+#         )
 
-        # Perform bulk update for all overdue actions
-        overdue_and_incomplete_actions.update(status="overdue")
+#         # Perform bulk update for all overdue actions
+#         overdue_and_incomplete_actions.update(status="overdue")
 
-    except Exception as e:
-        # Retry the task if it fails due to a network error or other
-        # resource availability error
-        update_overdue_statuses.retry(exc=e)
+#     except Exception as e:
+#         # Retry the task if it fails due to a network error or other
+#         # resource availability error
+#         update_overdue_statuses.retry(exc=e)
