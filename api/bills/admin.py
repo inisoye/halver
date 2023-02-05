81181debd81f3789ed7668da3bba1fd4bb5aad33
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-02-05 11:43:21 +0100

    feat: register bill models in admin

--- api/bills/admin.py
+++ api/bills/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from bills.models import Bill, BillAction, BillTransaction
+
+admin.site.register(Bill)
+admin.site.register(BillAction)
+admin.site.register(BillTransaction)
