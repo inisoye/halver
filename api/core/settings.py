e2c232f67e988c8f4276042a9174ec7dc50862e9
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-05-14 16:06:59 +0100

    fix: remove session auth

--- api/core/settings.py
+++ api/core/settings.py
@@ -213,7 +213,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_RENDERER_CLASSES": (
