aba3faf8725d7d3f780f29610be504cc0e4f2dff
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-06-19 10:00:36 +0100

    feat: add navigation themes

--- mobile/src/navigation/NavigationContainer.tsx
+++ mobile/src/navigation/NavigationContainer.tsx
@@ -1,22 +1,27 @@
 import { useFlipper } from '@react-navigation/devtools';
 import {
+  DarkTheme,
+  DefaultTheme,
   NavigationContainer as RNNavigationContainer,
   useNavigationContainerRef,
 } from '@react-navigation/native';
 import * as React from 'react';
 import { useMMKVString } from 'react-native-mmkv';
 
+import { Box } from '@/components';
 import { checkIfUserDetailsAreIncomplete, useUserDetails } from '@/features/account';
 import { useFullScreenLoader } from '@/hooks';
 import { apiClient, setAxiosDefaultToken } from '@/lib/axios';
 import { allMMKVKeys } from '@/lib/mmkv';
 import { LoginStackNavigator, OnboardingStackNavigator } from '@/navigation/stacks';
+import { useIsDarkMode } from '@/utils';
 
 import { AppRootStackNavigator } from './AppRootStackNavigator';
 
 export const NavigationContainer: React.FunctionComponent = () => {
   const [token] = useMMKVString(allMMKVKeys.token);
   const { data: userDetails, isLoading, isFetching } = useUserDetails();
+  const isDarkMode = useIsDarkMode();
 
   const navigationRef = useNavigationContainerRef();
   useFlipper(navigationRef);
@@ -36,8 +41,11 @@ export const NavigationContainer: React.FunctionComponent = () => {
   });
 
   return (
-    <>
-      <RNNavigationContainer ref={navigationRef}>
+    <Box backgroundColor="background" flex={1}>
+      <RNNavigationContainer
+        ref={navigationRef}
+        theme={isDarkMode ? DarkTheme : DefaultTheme}
+      >
         {!token || isLoading ? (
           <LoginStackNavigator />
         ) : areUserDetailsIncomplete ? (
@@ -46,6 +54,6 @@ export const NavigationContainer: React.FunctionComponent = () => {
           <AppRootStackNavigator />
         )}
       </RNNavigationContainer>
-    </>
+    </Box>
   );
 };
