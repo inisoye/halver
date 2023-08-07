f114bad2d4e259737b745fe10ac2e9cd529547e9
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-08-07 05:30:33 +0100

    feat: update packages and sentry config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import { useFonts } from 'expo-font';
+import * as Network from 'expo-network';
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { initializeMMKVFlipper } from 'react-native-mmkv-flipper-plugin';
@@ -7,16 +8,33 @@ import * as Sentry from 'sentry-expo';
 import { storage } from '@/lib/mmkv';
 import { NavigationContainer } from '@/navigation';
 import { Providers } from '@/providers';
-import { IS_DEV, IS_DEV_OR_PREVIEW } from '@/utils';
+import { isIOS } from '@/utils';
 
-if (__DEV__ || IS_DEV) {
+if (__DEV__) {
   initializeMMKVFlipper({ default: storage });
 }
 
+const devServerPort = 8081;
+let devServerIpAddress: string | null = null;
+Network.getIpAddressAsync().then(ip => {
+  devServerIpAddress = ip;
+});
+
 Sentry.init({
-  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-  enableInExpoDevelopment: true,
-  debug: IS_DEV_OR_PREVIEW, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  dsn: 'https://e7b0a97ab743aeee4cc1527e3d0423a8@o4505161164980224.ingest.sentry.io/4505657064423424',
+  enableInExpoDevelopment: isIOS(),
+  debug: __DEV__, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  tracesSampleRate: 1.0,
+  integrations: [
+    new Sentry.Native.ReactNativeTracing({
+      shouldCreateSpanForRequest: url => {
+        return (
+          !__DEV__ ||
+          !url.startsWith(`http://${devServerIpAddress}:${devServerPort}/logs`)
+        );
+      },
+    }),
+  ],
 });
 
 export default function App() {
