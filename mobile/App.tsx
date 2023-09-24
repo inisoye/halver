83cc9fa169c7c931d09f3986f9ce4b7ad1f38d9a
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-09-24 10:44:00 +0100

    feat: add notification sounds

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -18,7 +18,7 @@ import { useIsDarkModeSelected } from '@/utils';
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
+    shouldPlaySound: true,
     shouldSetBadge: false,
   }),
 });
