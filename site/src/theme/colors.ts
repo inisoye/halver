7847c277829d709437afbbe9c0f4a3e64329019f
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-10-19 11:21:58 +0100

    feat: add base styles and configuration

---
+++ site/src/theme/colors.ts
@@ -0,0 +1,199 @@
+export const grayLight = {
+  1: '#fcfcfc',
+  2: '#f9f9f9',
+  3: '#f0f0f0',
+  4: '#e8e8e8',
+  5: '#e0e0e0',
+  6: '#d9d9d9',
+  7: '#cecece',
+  8: '#bbbbbb',
+  9: '#8d8d8d',
+  10: '#838383',
+  11: '#646464',
+  12: '#202020',
+  mainBg: '#eeedee'
+}
+
+export const grayLightA = {
+  1: '#00000003',
+  2: '#00000006',
+  3: '#0000000f',
+  4: '#00000017',
+  5: '#0000001f',
+  6: '#00000026',
+  7: '#00000031',
+  8: '#00000044',
+  9: '#00000072',
+  10: '#0000007c',
+  11: '#0000009b',
+  12: '#000000df'
+}
+
+export const greenLight = {
+  1: '#fbfefc',
+  2: '#f4fbf6',
+  3: '#e6f6eb',
+  4: '#d6f1df',
+  5: '#c4e8d1',
+  6: '#adddc0',
+  7: '#8eceaa',
+  8: '#5bb98b',
+  9: '#30a46c',
+  10: '#2b9a66',
+  11: '#218358',
+  12: '#193b2d'
+}
+
+export const redLight = {
+  1: '#fffcfc',
+  2: '#fff7f7',
+  3: '#feebec',
+  4: '#ffdbdc',
+  5: '#ffcdce',
+  6: '#fdbdbe',
+  7: '#f4a9aa',
+  8: '#eb8e90',
+  9: '#e5484d',
+  10: '#dc3e42',
+  11: '#ce2c31',
+  12: '#641723'
+}
+
+export const yellowLight = {
+  1: '#fdfdf9',
+  2: '#fefce9',
+  3: '#fffab8',
+  4: '#fff394',
+  5: '#ffe770',
+  6: '#f3d768',
+  7: '#e4c767',
+  8: '#d5ae39',
+  9: '#ffe629',
+  10: '#ffdc00',
+  11: '#9e6c00',
+  12: '#473b1f'
+}
+
+export const grayDark = {
+  1: '#111111',
+  2: '#191919',
+  3: '#222222',
+  4: '#2a2a2a',
+  5: '#313131',
+  6: '#3a3a3a',
+  7: '#484848',
+  8: '#606060',
+  9: '#6e6e6e',
+  10: '#7b7b7b',
+  11: '#b4b4b4',
+  12: '#eeeeee'
+}
+
+export const grayDarkA = {
+  1: '#00000000',
+  2: '#ffffff09',
+  3: '#ffffff12',
+  4: '#ffffff1b',
+  5: '#ffffff22',
+  6: '#ffffff2c',
+  7: '#ffffff3b',
+  8: '#ffffff55',
+  9: '#ffffff64',
+  10: '#ffffff72',
+  11: '#ffffffaf',
+  12: '#ffffffed'
+}
+
+export const greenDark = {
+  1: '#0e1512',
+  2: '#121b17',
+  3: '#132d21',
+  4: '#113b29',
+  5: '#174933',
+  6: '#20573e',
+  7: '#28684a',
+  8: '#2f7c57',
+  9: '#30a46c',
+  10: '#33b074',
+  11: '#3dd68c',
+  12: '#b1f1cb'
+}
+export const redDark = {
+  1: '#191111',
+  2: '#201314',
+  3: '#3b1219',
+  4: '#500f1c',
+  5: '#611623',
+  6: '#72232d',
+  7: '#8c333a',
+  8: '#b54548',
+  9: '#e5484d',
+  10: '#ec5d5e',
+  11: '#ff9592',
+  12: '#ffd1d9'
+}
+
+export const yellowDark = {
+  1: '#14120b',
+  2: '#1b180f',
+  3: '#2d2305',
+  4: '#362b00',
+  5: '#433500',
+  6: '#524202',
+  7: '#665417',
+  8: '#836a21',
+  9: '#ffe629',
+  10: '#ffff57',
+  11: '#f5e147',
+  12: '#f6eeb4'
+}
+
+export const brand = {
+  'apricot-1': '#FEF5F3',
+  'apricot-2': '#FCE9E6',
+  'apricot-3': '#F8D1CA',
+  'apricot-4': '#F5B9AF',
+  'apricot-5': '#F1A294',
+  'apricot-6': '#EE8A79',
+  'apricot-7': '#EC7E6B',
+  'apricot-8': '#EB725E',
+  'apricot-9': '#E96650',
+  'apricot-10': '#E75B43',
+  'apricot-11': '#E6553C',
+
+  'casal-1': '#EFF6F7',
+  'casal-2': '#DBEAED',
+  'casal-3': '#B1D3D9',
+  'casal-4': '#88BCC5',
+  'casal-5': '#5FA5B1',
+  'casal-6': '#45838E',
+  'casal-7': '#315D65',
+  'casal-8': '#2A5057',
+  'casal-9': '#24444A',
+  'casal-10': '#1D373C',
+  'casal-11': '#1A3135',
+
+  'goldDrop-1': '#FFFAF0',
+  'goldDrop-2': '#FFEED2',
+  'goldDrop-3': '#FFD895',
+  'goldDrop-4': '#FFC157',
+  'goldDrop-5': '#FFAB1A',
+  'goldDrop-6': '#DC8B00',
+  'goldDrop-7': '#B37100',
+  'goldDrop-8': '#8A5700',
+  'goldDrop-9': '#623E00',
+  'goldDrop-10': '#392400',
+  'goldDrop-11': '#241700',
+
+  'pharlap-1': '#F0EBEB',
+  'pharlap-2': '#E7DFE0',
+  'pharlap-3': '#D5C8C9',
+  'pharlap-4': '#C3B1B2',
+  'pharlap-5': '#B29A9C',
+  'pharlap-6': '#A08385',
+  'pharlap-7': '#916F71',
+  'pharlap-8': '#7C5F61',
+  'pharlap-9': '#685051',
+  'pharlap-10': '#544042',
+  'pharlap-11': '#4A393A'
+}
