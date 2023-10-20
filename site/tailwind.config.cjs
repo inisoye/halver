2a593af67e76d68eed7f1622bf538119476e10d3
Inioluwa Akinyosoye <inisoye@gmail.com>
2023-10-20 09:18:06 +0100

    fix: correct scroll sticky effect

--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -1,8 +1,13 @@
+import * as colors from './src/theme/colors'
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: colors
+		},
 	},
 	plugins: [],
 }
